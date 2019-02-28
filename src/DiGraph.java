c514156fb63f47a51abeb7ecb8513b851c98036b
Xin Li <bulixin@bu.edu>
2019-02-28 05:17:03 -0500

    Created Java Directed Graph class DiGraph() using Java Collections.

---
+++ src/DiGraph.java
@@ -0,0 +1,139 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * <h1>class DiGraph</h1>
+ *
+ * <p>
+ *     Define a directed graph using <em>Adjacency List Representation</em>.
+ * </p>
+ *
+ */
+public class DiGraph {
+    private Map<Node, List<Node>> adjacentNodes;
+
+    /**
+     * <h2>Class constructor.</h2>
+     */
+    DiGraph() {
+        this.adjacentNodes = new HashMap<Node, List<Node>>();
+    }
+
+    /**
+     * <h2>void addNode(String label)</h2>
+     * <p>
+     *     This method adds nodes to the graph with given label when the node is absent.
+     * </p>
+     * @param label label attached to the node to be added
+     */
+    void addNode(String label) {
+        adjacentNodes.putIfAbsent(new Node(label), new ArrayList<>());
+    }
+
+    /**
+     * <h2>void removeNode(String label)</h2>
+     * <p>
+     *     This method removes a node from the graph with given label.
+     * </p>
+     * @param label label attached to the node to be removed
+     */
+    void removeNode(String label) {
+        Node n = new Node(label);
+        adjacentNodes.values().stream().map(e -> e.remove(n)).collect(Collectors.toList());
+        adjacentNodes.remove(new Node(label));
+    }
+
+    /**
+     * <h2>void addArc(String label1, String label2)</h2>
+     * <p>
+     *     This method adds a directed arc to the graph with given start node and end node label.
+     * </p>
+     * @param label1 start node label
+     * @param label2 end node label
+     */
+    void addArc(String label1, String label2) {
+        Node n1 = new Node(label1);
+        Node n2 = new Node(label2);
+        adjacentNodes.get(n1).add(n2);
+    }
+
+    /**
+     * <h2>void removeArc(String label1, String label2)</h2>
+     * <p>
+     *     This method removes a directed arc from the graph with given start node and end node labels.
+     * </p>
+     * @param label1 start node label
+     * @param label2 end node label
+     */
+    void removeArc(String label1, String label2) {
+        Node n1 = new Node(label1);
+        Node n2 = new Node(label2);
+        List<Node> n1Neighbors = adjacentNodes.get(n1);
+        if (n1Neighbors != null)
+            n1Neighbors.remove(n2);
+    }
+
+    /**
+     * <h2>List<Node> getNeighbors(String label)</h2>
+     * <p>
+     *     This method can get the neighbor nodes of a particular node.
+     * </p>
+     * @param label query node label
+     * @return a list of neighbor nodes of given node
+     */
+    List<Node> getNeighbors(String label) {
+        return adjacentNodes.get(new Node(label));
+    }
+
+    String printGraph() {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (Node n: adjacentNodes.keySet()) {
+            if (!adjacentNodes.get(n).isEmpty()){
+                stringBuffer.append(n);
+                stringBuffer.append(" -> ");
+                stringBuffer.append(adjacentNodes.get(n).get(0));
+                stringBuffer.append('\n');
+            }
+        }
+
+        return stringBuffer.toString();
+    }
+
+    /**
+     * <h2>class Node</h2>
+     * <p>A simple node class identified by a String label.</p>
+     */
+    class Node {
+        String label;
+
+        Node(String label) {
+            this.label = label;
+        }
+        @Override
+        public boolean equals(Object object) {
+            Node node = (Node) object;
+            return node.label == label;
+        }
+        @Override
+        public int hashCode() {
+            return label.hashCode();
+        }
+        @Override
+        public String toString() {
+            return label;
+        }
+    }
+
+    public static void main(String[] args) {
+        DiGraph DG = new DiGraph();
+        DG.addNode("Xin Li");
+        DG.addNode("Xueying Wang");
+        DG.addArc("Xin Li", "Xueying Wang");
+        DG.addArc("Xueying Wang", "Xin Li");
+
+        System.out.println(DG.printGraph());
+    }
+}
